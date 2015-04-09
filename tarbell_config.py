7707fdf99100a1c0aeaa9a0c2a117c310fd34b9e
J. Heasly <jheasly@earthlink.net>
2015-04-09 16:51:49 -0700

    First attempt at breweries, festivals.

--- tarbell_config.py
+++ tarbell_config.py
@@ -36,7 +36,7 @@ S3_BUCKETS = {
     # then use tarbell publish mytarget to publish to it
 
     "staging": "uploads.registerguard.com/discovery",
-    "production":  "cloud.registerguard.com/discovery",
+    "production": "cloud.registerguard.com/discovery",
 }
 
 # Default template variables
@@ -61,8 +61,23 @@ from flask import Blueprint, Response, g, render_template
 
 blueprint = Blueprint('discovery', __name__)
 
+@blueprint.route('/breweries/print/')
+def breweries_indesign_tagged_text():
+    id_file_timestamp = '%s' % datetime.datetime.now().strftime('%Y-%m-%d-%I-%M-%S-%p')
+
+    context = g.current_site.get_context()
+    context['os'] = 'WIN'
+    the_text = render_template('breweries/print.html', **context)
+    # make the line endings Windows
+    the_text = the_text.replace(u'\n', u'\r\n')
+    # make it utf-16le, what Windows calls 'Unicode'
+    the_text = the_text.encode('utf-16-le')
+    the_response = Response(the_text, mimetype='text/plain')
+    the_response.headers['Content-Disposition'] = 'attachment; filename=breweries-%s.txt' % id_file_timestamp
+    return the_response
+
 @blueprint.route('/campgrounds/print/')
-def indesign_tagged_text():
+def campgrounds_indesign_tagged_text():
     id_file_timestamp = '%s' % datetime.datetime.now().strftime('%Y-%m-%d-%I-%M-%S-%p')
 
     context = g.current_site.get_context()
@@ -74,4 +89,19 @@ def indesign_tagged_text():
     the_text = the_text.encode('utf-16-le')
     the_response = Response(the_text, mimetype='text/plain')
     the_response.headers['Content-Disposition'] = 'attachment; filename=campgrounds-%s.txt' % id_file_timestamp
+    return the_response
+
+@blueprint.route('/festivals/print/')
+def festivals_indesign_tagged_text():
+    id_file_timestamp = '%s' % datetime.datetime.now().strftime('%Y-%m-%d-%I-%M-%S-%p')
+
+    context = g.current_site.get_context()
+    context['os'] = 'WIN'
+    the_text = render_template('festivals/print.html', **context)
+    # make the line endings Windows
+    the_text = the_text.replace(u'\n', u'\r\n')
+    # make it utf-16le, what Windows calls 'Unicode'
+    the_text = the_text.encode('utf-16-le')
+    the_response = Response(the_text, mimetype='text/plain')
+    the_response.headers['Content-Disposition'] = 'attachment; filename=festivals-%s.txt' % id_file_timestamp
     return the_response
